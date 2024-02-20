70d4bac0bfcb645d1d348bea66e9a219893a4a8e
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-20 20:51:06 +0000

    added core pages needed for base functionality

--- group2/questAI/urls.py
+++ group2/questAI/urls.py
@@ -5,5 +5,11 @@ app_name = 'questAI'
 
 urlpatterns = [
 path('', views.index, name='index'),
+path('home/', views.home, name='home'),
+path('login/', views.login, name='login'),
+path('signup/', views.signup, name='signup'),
+path('basket/', views.basket, name='basket'),
+path('topup/', views.topup, name='topup'),
+path('checkout/', views.checkout, name='checkout')
 ]
 
--- group2/questAI/views.py
+++ group2/questAI/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'questAI/index.html')
 
+def home(request):
+    return HttpResponse("Test for Home Page")
+
+def login(request):
+    return HttpResponse("Test for Login Page")
+
+def signup(request):
+    return HttpResponse("Test for Sign-Up Page")
+
+def basket(request):
+    return HttpResponse("Test for Basket Page")
+
+def topup(request):
+    return HttpResponse("Test for Top Up Page")
+
+def checkout(request):
+    return HttpResponse("Test for Checkout Page")
+
