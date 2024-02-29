196cc51afd213da18b48f5fd36dc08d9788fb49b
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-29 09:02:33 +0000

    small change to models

--- group2/group2/urls.py
+++ group2/group2/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from questAI import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name = 'index'),
-    path('questAI/', include('questAI.urls'))
-]
+    path('questAI/', include('questAI.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
