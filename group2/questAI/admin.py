85442c37744d7733b560fd251a12395b91d21223
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-29 09:07:54 +0000

    added models to admin.py

--- group2/questAI/admin.py
+++ group2/questAI/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from questAI.models import Products, Baskets, Comments, Reviews
+
 
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(Comments)
+admin.site.register(Reviews)
+admin.site.register(Baskets)
