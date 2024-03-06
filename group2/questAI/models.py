bfeb621d2a31680cc23f1e384f527960ea99a93c
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-03-06 00:21:50 +0000

    changed model for basket, made it auto unique

--- group2/questAI/models.py
+++ group2/questAI/models.py
@@ -15,7 +15,7 @@ class Products (models.Model):
         return self.productName
 
 class Baskets (models.Model):
-    basketId = models.IntegerField(unique=True)
+    productId = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     productId =models.ForeignKey(Products, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6,decimal_places=2)
